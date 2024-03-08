4bd934121aa0617668e5cc75a6a44758042c75d0
dev-umair-ali <umair.ali@aimnode.com>
2024-03-08 15:25:55 +0500

    Updated Code push

--- src/Compnent/Home/Sections/Sectoion4.jsx
+++ src/Compnent/Home/Sections/Sectoion4.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import "../Styles/Section4.css";
-
+import Carousel4 from "./Carousel4";
 const Sectoion4 = () => {
   return (
     <div className="section4">
-      <div className="section4_left">
-      <img src="./data/mostsale.png" alt="" />
-      </div>
-      <div className="most_sale">
-        <h2>Top Selling</h2>
+      <div className="section-4left">
+        <h1>
+          WE THINK YOU'LL <br />
+          LOVE THESE
+        </h1>
         <p>
-          It has survived not only five centuries, but also the leap into
-          electronic typesetting.
+          Welcome to TiatailToys, where joy reigns supreme! Dive into a world of
+          enchantment with our Bubble Bath Toys, experience precision play with
+          the Dart Shooter, and find warmth in our timeless Teddy Bears. Quality
+          and safety are our top priorities, ensuring every TiatailToy is a
+          trusted companion in the journey of childhood. Come, explore, and
+          embrace the magic at TiatailToys – where playtime dreams come true
         </p>
-        <img src="./data/mostsae2.png" alt="" />
-        <h3>Eid Card: $15</h3>
-        <button>20k+ Sold</button>
+        <button>
+          {" "}
+          <h2>View all</h2>{" "}
+        </button>
+      </div>
+      <div className="section-4right">
+        <Carousel4 />
       </div>
     </div>
   );
